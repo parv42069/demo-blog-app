66785f3be4b52600377757a957797e783b857f5d
parv42069 <109207365+parv42069@users.noreply.github.com>
2023-01-09 04:52:13 +0530

    add mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,6 +4,7 @@ const {
   GraphQLString,
   GraphQLList,
   GraphQLSchema,
+  GraphQLNonNull,
 } = require('graphql');
 
 const Author = require('../models/Author');
@@ -65,6 +66,81 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const author = new Author({
+          name: args.name,
+          email: args.email,
+        });
+        return author.save();
+      },
+    },
+    deleteAuthor: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Author.findByIdAndRemove(args.id);
+      },
+    },
+    addPost: {
+      type: PostType,
+      args: {
+        title: { type: GraphQLNonNull(GraphQLString) },
+        content: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        const post = new Post({
+          title: args.title,
+          content: args.content,
+          authorId: args.authorId,
+        });
+        return post.save();
+      },
+    },
+    deletePost: {
+      type: PostType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Post.findByIdAndRemove(args.id);
+      },
+    },
+    updatePost: {
+      type: PostType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        title: { type: GraphQLString },
+        content: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        return Post.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              title: args.title,
+              content: args.content,
+            },
+          },
+          { new: true }
+        );
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
